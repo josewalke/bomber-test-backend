a297971619fa27a4c33ba34c435dc25cdc0122f9
Jose Perez <josejperez@singularfactory.net>
2020-04-30 17:48:53 +0100

    actualizando

---
+++ controlers/url_clase.controller.js
@@ -0,0 +1,37 @@
+const url_claseModel = require("../models/url_clase.model");
+
+module.exports = {
+  postURL,
+  getURL,
+  putURL
+};
+
+function postURL(req,res){
+  var body = {
+    direccion: 'sin direccion',
+    contraseña: 'sin contraseña'
+  }
+  url_claseModel
+    .create(body)
+    .then(response => res.json(response))
+    .catch(err => {
+      res.status(403).json({ error: err });
+    });
+}
+
+async function getURL(req,res){
+  url_claseModel
+  .find()
+  .then(response => res.json(response))
+    .catch(err => {
+      res.status(403).json({ error: err });
+    });
+}
+
+async function putURL(req,res){
+  console.log(req.body)
+  url_claseModel
+  .findByIdAndUpdate(req.params.id, req.body)
+    .then(response => res.json('actualizado correctamente'))
+    .catch((err) => handdleError(err, res))
+}
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const correoRouter = require("./correo.router");
 const suscriptionRouter = require("./suscription.router");
 const localizacionRouter = require("./localizacion.router");
 const paymentStripe = require("./paymentStripe.router");
+const url_clase = require("./url_clase.router");
 
 router.use("/auth", authRouter);
 router.use("/users", usersRouter);
@@ -21,4 +22,6 @@ router.use("/correo", correoRouter);
 router.use("/suscription", suscriptionRouter);
 router.use("/localizacion", localizacionRouter);
 router.use("/paymentStripe", paymentStripe);
+router.use("/url_clase",url_clase);
+
 module.exports = router;
