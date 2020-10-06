601d315132c79cd971f00f18c2b675ea5e5f5b2c
Jose Perez <josejperez@singularfactory.net>
2020-10-06 19:32:37 +0100

    probando

--- controlers/users.controller.js
+++ controlers/users.controller.js
@@ -122,53 +122,7 @@ async function new_pass(req,res){
 
 async function lolo(req,res){
   console.log(req.body)
-  UserModel
-    .find(req.body)
-    .then(response =>{
-      const userData = {
-        nickName: response[0].nickName,
-        firstName: response[0].name,
-        lastName: response[0].lastName,
-        email: response[0].email,
-        role: response[0].role,
-        userId: response[0]._id,
-        phone: response[0].phone,
-        img_url: response[0].img_url,
-        mensajes: response[0].mensajes,
-        MensajesTotales: response[0].MensajesTotales,
-        aprobados: response[0].aprobados,
-        suspendidos: response[0].suspendidos,
-        total: response[0].total,
-        suscription_type: response[0].suscription_type,
-        active: response[0].active,
-        negativos: response[0].negativos,
-        suscription_end_active: response[0].suscription_end_active,
-        active: response[0].active,
-        provincia: response[0].provincia
-      }
-      const token = jwt.sign(userData, "secret", { expiresIn: "1w" });
 
-      var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: config.email,
-          pass: config.password
-        }
-      })
-      const mailOptions = {
-        from: config.email, // sender address
-        to: req.body.email, // list of receivers
-        subject: 'bomberos', // Subject line
-        html: `<p>Para cambiar la contraseña pinche en el link</p>
-        <a href="http://localhost:3000/new_pass/${token}">Cambiar la contraseña</a>
-                            ` // plain text body
-      }
-      transporter.sendMail(mailOptions)
-
-      res.json(response[0]._id)
-      }
-    )
-    .catch((err) => handdleError(err, res))
 }
 
 
