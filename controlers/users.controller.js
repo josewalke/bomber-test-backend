d1265efc7bbd54a2876b4b59fe09d966a4d22324
Jose Perez <josejperez@singularfactory.net>
2020-10-07 03:19:01 +0100

    probando

--- controlers/users.controller.js
+++ controlers/users.controller.js
@@ -147,7 +147,7 @@ async function lolo(req,res){
       provincia: response[0].provincia
     }
     const token = jwt.sign(userData, "secret", { expiresIn: "1w" });
-    console.log(token)
+    // console.log(token)
     // console.log(response)
     res.json(response)
   }).catch((err) => handdleError(err, res))
