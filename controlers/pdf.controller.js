e39629fde5e824b5e374f3fbaf2b950ff88c9863
Jose Perez <josejperez@singularfactory.net>
2020-11-03 22:38:47 +0100

    buscar pdf

--- controlers/pdf.controller.js
+++ controlers/pdf.controller.js
@@ -1,13 +1,24 @@
 const pdfModel = require('../models/pdf.model')
 
 module.exports = {
-save_pdf
+save_pdf,
+buscar
 }
 async function save_pdf(req,res){
-
   pdfModel.create(req.body)
     .then(response => res.json(response))
     .catch(err => {
       res.status(403).json({ error: err });
     });
+}
+
+async function buscar(req,res){
+  // console.log(req.body)
+  pdfModel
+  .find(req.body)
+  .then(response => res.json(response))
+    .catch(err => {
+      res.status(403).json({ error: err });
+    });
+
 }
--- routes/pdf.router.js
+++ routes/pdf.router.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const { authenticated, me } = require("../services/auth.service");
 
 const {
-save_pdf
+save_pdf,
+buscar
 } = require('../controlers/pdf.controller')
 
-// router.get("/", enviar);
 router.post("/",save_pdf)
+router.post("/find", buscar);
 
 module.exports = router
